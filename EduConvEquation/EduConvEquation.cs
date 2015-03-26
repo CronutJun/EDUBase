7b272721f1709246b17735a91ff66fda63bceb74
Cronut <lion001@daum.net>
2015-03-26 16:00:18 +0900

    중간저

--- EduConvEquation/EduConvEquation.cs
+++ EduConvEquation/EduConvEquation.cs
@@ -4,61 +4,121 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Collections.Generic;
 
 namespace EduConvEquation
 {
     public class ConvEquation
     {
+        private static int HEADER_LEN = 14;
+
+        private byte[] bufMTEFHeader = { 0x21, 0xFF, 0x0B, 0x4D, 0x61, 0x74, 0x68, 0x54, 0x79, 0x70, 0x65, 0x30, 0x30, 0x31 };
+        private List<byte[]> LstBlock = new List<byte[]>();
+
         public String Convert(String path)
         {
-            if (!File.Exists(path))
+            if( !File.Exists(path) )
                 return "File does not exist.";
             else
             {
                 FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
-                fs.Seek(0, SeekOrigin.Begin);
-                fs.ReadByte();
-                String stringToLookFor = "ABC";
-                byte[] bufferToLookFor = { 0x21, 0xFF, 0x0B };
 
-                int matchCounter = 1; // count matches for nicer output
-
-                byte[] readBuffer = new byte[16384]; // our input buffer
-                int bytesRead = 0; // number of bytes read
-                int offset = 0; // offset inside read-buffer
-                long filePos = 0; // position inside the file before read operation
-                while( (bytesRead = fs.Read(readBuffer, offset, readBuffer.Length-offset)) > 0 )
+                long findPos = findMatch(fs, bufMTEFHeader);
+                if (findPos >= 0)
                 {
-                    for( int i=0; i<bytesRead+offset-bufferToLookFor.Length; i++ )
-                    {
-                        bool match = true;
-                        for (int j=0; j<bufferToLookFor.Length; j++)
-                            if (bufferToLookFor[j] != readBuffer[i+j])
-                            {
-                                match = false;
-                                break;
-                            }
-                        if( match )
-                        {
-                            return path;
-                        }
-                    }
-                    // store file position before next read
-                    filePos = fs.Position;
-
-                    // store the last few characters to ensure matches on "chunk boundaries"
-                    offset = bufferToLookFor.Length;
-                    for (int i=0; i<offset; i++)
-                        readBuffer[i] = readBuffer[readBuffer.Length-offset+i];
-                }   
-                Console.WriteLine("No match found");
-                return "No match found";
+                    Console.WriteLine("Found. position = {0}", findPos);
+                    CollectBlock(fs, findPos + HEADER_LEN);
+                    return path;
+                }
+                else
+                {
+                    return "This GIF file is not a type of the MathType.";
+                }
             }
         }
 
         public String Convert(FileStream stream)
         {
-            return "ABC";
+            long findPos = findMatch(stream, bufMTEFHeader);
+            if (findPos >= 0)
+            {
+                Console.WriteLine("Found. position = {0}", findPos);
+                return "Found";
+            }
+            else
+            {
+                return "Not found.";
+            }
         }
+
+        private long findMatch(FileStream fs, byte[] bufferToLookFor)
+        {
+            byte[] readBuffer = new byte[16384]; // our input buffer
+            //byte[] readBuffer = new byte[100]; // our input buffer
+            int bytesRead = 0; // number of bytes read
+            int offset = 0;    // offset inside read-buffer
+            long filePos = 0;  // position inside the file before read operation
+            while ((bytesRead = fs.Read(readBuffer, offset, readBuffer.Length - offset)) > 0)
+            {
+                for( int i = 0; i < bytesRead + offset - bufferToLookFor.Length; i++ )
+                {
+                    bool match = true;
+                    for (int j = 0; j < bufferToLookFor.Length; j++)
+                        if (bufferToLookFor[j] != readBuffer[i + j])
+                        {
+                            match = false;
+                            break;
+                        }
+                    if (match)
+                    {
+                        return filePos + i - offset;
+                    }
+                }
+                // store file position before next read
+                filePos = fs.Position;
+
+                // store the last few characters to ensure matches on "chunk boundaries"
+                offset = bufferToLookFor.Length;
+                for (int i = 0; i < offset; i++)
+                    readBuffer[i] = readBuffer[readBuffer.Length - offset + i];
+            }
+            return -1;
+        }
+
+        private void CollectBlock(FileStream fs, long startPos)
+        {
+            fs.Seek(startPos, SeekOrigin.Begin);
+            int sizeBlock    = 0;
+            int totSizeBlock = 0;
+            LstBlock.Clear();
+            byte[] totBlock = null;
+            while( (sizeBlock = fs.ReadByte()) > 0 )
+            {
+                Console.WriteLine("Size of Block = {0}", sizeBlock);
+                byte[] blockData = new byte[sizeBlock];
+                fs.Read(blockData, 0, sizeBlock);
+                LstBlock.Add(blockData);
+                totSizeBlock += sizeBlock;
+            }
+            Console.WriteLine("Count of block = {0}", LstBlock.Count);
+            if (LstBlock.Count > 0)
+            {
+                totBlock = new byte[totSizeBlock];
+                int destOffset = 0;
+                foreach (byte[] elem in LstBlock)
+                {
+                    Console.WriteLine("elem size = {0}", elem.Length);
+                    System.Buffer.BlockCopy(elem, 0, totBlock, destOffset, elem.Length);
+                    destOffset += elem.Length;
+                }
+                // Print Hex String
+                for (int i = 0; i < totBlock.Length; i++)
+                    if ((i + 1) % 16 == 0)
+                        Console.WriteLine("{0:X2}", totBlock[i]);
+                    else
+                        Console.Write("{0:X2} ", totBlock[i]);
+            }
+        }
+
     }
 }
